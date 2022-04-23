5c7834f7da3ac77d265e8e5c2e4d4cf2c32db238
Paula Lopes <pauladev21@gmail.com>
2022-04-22 21:17:37 -0300

    experiment(...) implemented, 2 tests working, 1 failed

--- prob_calculator.py
+++ prob_calculator.py
@@ -1,8 +1,6 @@
 import copy
 import random
 
-from matplotlib import colors
-# Consider using the modules imported above.
 
 
 class Hat:
@@ -18,16 +16,59 @@ class Hat:
         return random.sample(self.contents, k=quant_balls)
 
 
+def experiment(hat, expected_balls, num_balls_drawn, num_experiments):
+    if total_balls(expected_balls) > num_balls_drawn:
+        print("Drawing too few balls, draw more")
+        return 0 
+    
+    equal_expected=0
+
+    for i in range(num_experiments):
+        balls = hat.draw(num_balls_drawn)
+        drawn_balls  = count_balls_by_color(balls)
+        if contains(drawn_balls, expected_balls):
+            equal_expected += 1
+        
+    return equal_expected/num_experiments
+
+def total_balls(obj):
+    total = 0
+    for color in obj:
+        total += obj[color] 
+    return total
 
 
+def count_balls_by_color(balls):
+    content = {}
+    for ball in balls:
+        if ball in content.keys():
+            content[ball] += 1
+        else:
+            content[ball] = 1
+    return content
 
-# For this project, you will write a program to determine the approximate probability of drawing certain balls randomly from a hat.
+def contains(drawn_balls, expected_balls):
+    if len(drawn_balls.keys()) < len(expected_balls.keys()):
+        return False
 
+    found_balls={}
+    for color in expected_balls:
+        if color in drawn_balls.keys():
+            if drawn_balls[color] < expected_balls[color]:
+                return False
+            else:
+                found_balls[color] = expected_balls[color]
+        else:
+            return False
+    
+    if found_balls == expected_balls:
+        return True
 
-# Next, create an experiment function in prob_calculator.py(not inside the Hat class). This function should accept the following arguments:
+    return False
 
-# hat: A hat object containing balls that should be copied inside the function.
-# expected_balls: An object indicating the exact group of balls to attempt to draw from the hat for the experiment. For example, to determine the probability of drawing 2 blue balls and 1 red ball from the hat, set expected_balls to {"blue": 2, "red": 1}.
-# num_balls_drawn: The number of balls to draw out of the hat in each experiment.
-# num_experiments: The number of experiments to perform. (The more experiments performed, the more accurate the approximate probability will be.)
-# The experiment function should return a probability.
+#---------------------QUICK TEST---------------------------
+h = Hat(blue=10, red=3, orange=8)
+
+for i in range(6):
+    n_exp = 10 ** (i + 1) 
+    print(experiment(h, {"blue": 2, "red": 1}, 5, n_exp))
